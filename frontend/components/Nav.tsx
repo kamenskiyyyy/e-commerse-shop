c7d138a35a29da7a58ee7f0ec0d2cf8a7615eb10
Nikolay Kamenev <kamenik1@icloud.com>
2022-06-28 15:46:10 +0300

    feat: Animating the Cart Cart Value

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -3,6 +3,7 @@ import NavStyles from '@components/styles/NavStyles';
 import { useUser } from '@components/User';
 import { SignOut } from '@components/SignOut';
 import { useCart } from '@lib/cartState';
+import CartCount from '@components/CartCount';
 
 export const Nav = () => {
   const user = useUser();
@@ -19,6 +20,12 @@ export const Nav = () => {
           <SignOut />
           <button type='button' onClick={openCart}>
             My Cart
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
           </button>
         </>
       ) : (
